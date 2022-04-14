49d2febc0965131986d21acd168e40f3554d9761
Raymond Broome <rbroome@gmail.com>
2022-04-14 00:14:00 -0500

    Implement all the required tasks

---
+++ src/components/Post/Post.js
@@ -0,0 +1,14 @@
+import './Post.css'
+
+const Post = (props) => {
+    
+    return (
+        <div className='post'>
+            <p>ID: {props.id}</p>
+            <p>Title: {props.title}</p>
+            <p>Author: {props.author}</p>
+        </div>
+    )
+}
+
+export default Post;
